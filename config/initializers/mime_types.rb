717f98a322b5f92ddbad1aaac4c690044047b08e
Sandeep Hebbar <sandeeph@gmail.com>
2015-09-30 16:47:06 +0530

    redone aws and s3

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,4 +3,5 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 Mime::Type.register "video/mp4", :mp4
-Mime::Type.register "application/vnd.ms-excel", :xlsx
+#Mime::Type.register "application/vnd.ms-excel", :xlsx
+Mime::Type.register "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", :xlsx
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,8 @@
-#Paperclip::Attachment.default_options[:url] = 'opimas.s3.amazonaws.com'
-#Paperclip::Attachment.default_options[:url] = 'reports/:id/:filename'
-#Paperclip::Attachment.default_options[:path] = 'reports/:id/:filename'
-#Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+if ENV['RAILS_ENV'] == "production"
+  Paperclip::Attachment.default_options[:url] = 'opimas-production.s3.amazonaws.com'
+else
+  Paperclip::Attachment.default_options[:url] = 'opimas.s3.amazonaws.com'
+end
+Paperclip::Attachment.default_options[:url] = 'reports/:id/:filename'
+Paperclip::Attachment.default_options[:path] = 'reports/:id/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
